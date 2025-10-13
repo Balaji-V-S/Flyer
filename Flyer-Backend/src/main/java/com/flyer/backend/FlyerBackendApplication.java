d53480b6a6860c1c0818a24ce02664f23b294b44
Balaji-V-S <vsbrobot@gmail.com>
2025-10-13 22:00:01 +0530

    chore: Isolated structure for better S/W design & extension

--- Flyer-Backend/src/main/java/com/flyer/backend/FlyerBackendApplication.java
+++ Flyer-Backend/src/main/java/com/flyer/backend/FlyerBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlyerBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FlyerBackendApplication.class, args);
 	}
-
 }
---
+++ Flyer-Backend/src/main/java/com/flyer/backend/entity/UserData.java
@@ -0,0 +1,4 @@
+package com.flyer.backend.entity;
+
+public class UserData {
+}
